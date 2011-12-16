b05548d2b95d9aa44fcc37a11f111383235de611
andydenenberg <andydenenberg@gmail.com>
2011-12-16 14:27:22 -0600

    without YAML

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,19 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 StinV4::Application.initialize!
 
+# config = YAML.load(File.read('../../../desktop/config.yml'))   
+
+ActionMailer::Base.smtp_settings = {
+#  :user_name =>  config['user_name'],
+#  :password => config['password'],
+  :user_name =>  ENV['SENDGRID_USERNAME'] ,
+  :password => ENV['SENDGRID_PASSWORD'] ,
+  :domain => "ospreypointpartners.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 
 # http://guides.rubyonrails.org/action_mailer_basics.html#walkthrough-to-generating-a-mailer
 
