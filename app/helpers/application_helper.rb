5470ac9481e28c8dded656571583d2830492527c
andydenenberg <andydenenberg@gmail.com>
2011-12-19 02:57:06 -0600

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,11 +19,11 @@ module ApplicationHelper
 	end
 	
 	def show_photo(userp,size)
-	  if userp.avatar.file?
-		  image_tag userp.avatar.url(size)
-	  else
-		  data = 'No Photo on file'
-    end	
+#	  if userp.avatar.file?
+#		  image_tag userp.avatar.url(size)
+#	  else
+#		  data = 'No Photo on file'
+#    end	
     
   end
 	
