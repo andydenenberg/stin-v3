3a554b827505205f37c9b76ee8c15717cd95c64c
andydenenberg <andydenenberg@gmail.com>
2011-12-19 23:09:23 -0600

    beta3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 StinV4::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+config.action_mailer.default_url_options = { :host => 'stin-v3.heroku.com' }
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
