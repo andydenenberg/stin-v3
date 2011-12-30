c76d4d7051d2b915401e8d755c315ce1e68f2a28
andydenenberg <andydenenberg@gmail.com>
2011-12-30 16:14:59 -0600

    rev 1.3

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -7,7 +7,7 @@ class Donation < ActiveRecord::Base
   validates_numericality_of :value
   
   
-  def self.total_donations(user_id = 'All', org_id = 'All')
+  def self.total_value(user_id = 'All', org_id = 'All')
     @donations = user_id == 'All' && org_id == 'All' ? self.all :
     @donations = user_id == 'All' && org_id != 'All' ? self.where(:org_id => org_id ) :
     @donations = user_id != 'All' && org_id == 'All' ? self.where(:user_id => user_id ) :
@@ -21,6 +21,17 @@ class Donation < ActiveRecord::Base
     return total_value
   end
   
+  def self.total_count(user_id = 'All', org_id = 'All')
+    @donations = user_id == 'All' && org_id == 'All' ? self.all :
+    @donations = user_id == 'All' && org_id != 'All' ? self.where(:org_id => org_id ) :
+    @donations = user_id != 'All' && org_id == 'All' ? self.where(:user_id => user_id ) :
+    @donations = user_id != 'All' && org_id != 'All' ? self.where(:user_id => user_id, :org_id => org_id )  :
+    never_get_here
+    
+    total_count = @donations.count
+    return total_count
+    
+  end
   
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ StinV4::Application.routes.draw do
   
 
   get "home/index"
+  get "home/show"
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
