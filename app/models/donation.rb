1e3c2fe890a6c279fa8edacce3ca96af2805cd19
andydenenberg <andydenenberg@gmail.com>
2011-12-30 15:08:18 -0600

    rev 1.2

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -6,4 +6,21 @@ class Donation < ActiveRecord::Base
   validates_presence_of :value
   validates_numericality_of :value
   
+  
+  def self.total_donations(user_id = 'All', org_id = 'All')
+    @donations = user_id == 'All' && org_id == 'All' ? self.all :
+    @donations = user_id == 'All' && org_id != 'All' ? self.where(:org_id => org_id ) :
+    @donations = user_id != 'All' && org_id == 'All' ? self.where(:user_id => user_id ) :
+    @donations = user_id != 'All' && org_id != 'All' ? self.where(:user_id => user_id, :org_id => org_id )  :
+    never_get_here
+    
+    total_value = 0
+    @donations.each do |donate|
+      total_value += donate.value
+    end
+    return total_value
+  end
+  
+  
+  
 end
